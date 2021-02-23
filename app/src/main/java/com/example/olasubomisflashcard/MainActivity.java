ebee97d99d1a23ec2c9c48569d6d132f18de5434
olasubomi-o <67253470+olasubomi-o@users.noreply.github.com>
2021-02-22 19:07:44 -0600

    Changed the background color, integrated a toggle feature

--- app/src/main/java/com/example/olasubomisflashcard/MainActivity.java
+++ app/src/main/java/com/example/olasubomisflashcard/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.olasubomisflashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+
+        });
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+
     }
 }
